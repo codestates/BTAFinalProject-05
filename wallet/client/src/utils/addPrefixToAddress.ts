6e817990ab9e24b8c7d72c2e24ab1299a1021bc2
Sam Rho <samrho.dev@gmail.com>
2022-11-02 15:21:14 +0900

    addPrefixToAddress comment out

--- wallet/client/src/utils/addPrefixToAddress.ts
+++ wallet/client/src/utils/addPrefixToAddress.ts
@@ -1,5 +1,7 @@
+// TODO: ERGO의 address system research
 const addPrefixToAddress = (address: string) => {
-    return /0x/.test(address) ? address : `0x${address}`;
+    // return /0x/.test(address) ? address : `0x${address}`;
+    return address;
 }
 
 export default addPrefixToAddress;
