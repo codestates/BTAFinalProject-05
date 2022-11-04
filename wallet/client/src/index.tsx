8d292fc8d00a21ac28e03b261ced55a194f91b70
Sam Rho <samrho.dev@gmail.com>
2022-11-05 01:44:59 +0900

    semi colon 추가

--- wallet/client/src/index.tsx
+++ wallet/client/src/index.tsx
@@ -11,7 +11,7 @@ import {worker} from "./api/browser";
             serviceWorker: {
                 url: '/mockServiceWorker.js',
             },
-        })
+        });
     }
     const root = ReactDOM.createRoot(
         document.getElementById('root') as HTMLElement
@@ -22,7 +22,7 @@ import {worker} from "./api/browser";
             <App />
         </React.StrictMode>
     );
-})()
+})();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
