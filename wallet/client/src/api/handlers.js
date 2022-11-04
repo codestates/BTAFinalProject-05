fe492bc6bc64e9b1155f23777964e21551c2bb7b
Sam Rho <samrho.dev@gmail.com>
2022-11-05 01:40:33 +0900

    msw 추가 for api mocking

---
+++ wallet/client/src/api/handlers.js
@@ -0,0 +1,23 @@
+import { rest } from 'msw';
+import {ERGO_ENDPOINTS} from '../constants';
+
+export const handlers = [
+    rest.post(ERGO_ENDPOINTS.WALLET.INIT, (req, res, ctx) => {
+        return res(
+            ctx.status(200),
+            ctx.json({mnemonic: "noise donor humble write furnace dilemma auto quit glide divide frown federal siege used draft"})
+        )
+    }),
+    rest.get(ERGO_ENDPOINTS.WALLET.STATUS, (req, res, ctx) => {
+        return res(
+            ctx.status(200),
+            ctx.json({
+                isInitialized: false,
+                isUnlocked: false,
+                changeAddress: "",
+                walletHeight: 0,
+                error: ""
+            }),
+        );
+    }),
+]
